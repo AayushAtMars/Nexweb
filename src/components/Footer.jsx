f932ae782cb7e29b2da116514b3d2bb41a2edb9a
AayushAtMars <yoaayush14@gmail.com>
2026-05-13 14:19:58 +0530

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "../assets/logo.png";
 import { Link } from "react-router-dom";
-import logo from "../assets/logo.jpeg"
+
 const Footer = () => {
   const links = {
     Services: [
